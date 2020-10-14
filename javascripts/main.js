86f600c68526e1e9a74c56510dbc62a2809a3d2c
syansky <syansky@yahoo.com>
2020-10-14 21:35:53 +0700

    Progress 3

---
+++ javascripts/main.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    $('#content-1').css('display', 'none');
+    $('#content-2').css('display', 'block');
+}
+
+function showHome() {
+    $('#content-1').css('display', 'block');
+    $('#content-2').css('display', 'none');
+}
