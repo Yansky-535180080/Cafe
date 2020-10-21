510c6ed650e566bb57aa7d5263760ebcd8186b1a
syansky <syansky@yahoo.com>
2020-10-22 03:04:01 +0700

    Progress 7 --Final

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,13 @@
 function showMenu() {
     $('#content-1').css('display', 'none');
     $('#content-2').css('display', 'block');
+    AOS.init()
 }
 
 function showHome() {
     $('#content-1').css('display', 'block');
     $('#content-2').css('display', 'none');
+    AOS.init()
 }
 
 function showAbout() {
