de183cecdcc9a8440a7629c73818428b96316e6e
syansky <syansky@yahoo.com>
2020-11-29 01:32:40 +0700

    Progress 5

--- models/member.js
+++ models/member.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const memberSchema = monggoose.Schema({
+const memberSchema = mongoose.Schema({
     name: String, 
     email: String, 
     password: String
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,16 +2,21 @@ function showMenu() {
     $('#content-1').css('display', 'none');
     $('#content-2').css('display', 'block');
     AOS.init()
-}
+};
 
 function showHome() {
     $('#content-1').css('display', 'block');
     $('#content-2').css('display', 'none');
     AOS.init()
-}
+};
 
 function showAbout() {
     if ($('#content-1').css('display') == 'none') {
         showHome();
     }
-}
+};
+
+$(window).on("load", () => {
+    $('[data-toggle="popover"]').popover('show');
+    setTimeout(() => {$('[data-toggle="popover"]').popover('hide')}, 5000);
+});
