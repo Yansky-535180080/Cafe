b1c4e47cbfd18024d8bb6757d49c2a2ba97e03a5
Yansky <114139682+yanskys@users.noreply.github.com>
2024-08-09 11:10:50 +0700

    Fix - remove heroku + add new config for mongo

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/caffe-cafe';
 
-mongoose.connect(
-    dbUrl,
-    { useNewUrlParser: true , 
-        useUnifiedTopology: true }
-);
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: process.env.MONGODB_USER,
+  pass: process.env.MONGODB_PASSWORD,
+});
 const db = mongoose.connection;
 
 db.once("open", () => {
